7ce9dca3b1400456b54d60745c6f8c9bfdb296be
vinicius <vinicius.goncalvestj@gmail.com>
2024-04-23 13:33:56 -0400

    ajuste copiar pasta correta

--- backend/src/main/java/com/vinicius/springvue/PagesResources.java
+++ backend/src/main/java/com/vinicius/springvue/PagesResources.java
@@ -1,12 +1,19 @@
 package com.vinicius.springvue;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-@RestController("/")
+@Controller("/")
 public class PagesResources {
+//    @GetMapping
+//    public String index() {
+//        return "Hello World";
+//    }
+
     @GetMapping
-    public String index() {
-        return "Hello World";
+    public ModelAndView home() {
+        return new ModelAndView("/contabil/index.html");
     }
 }
